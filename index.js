7d30e52fedd249922bbeba965eafb8b5b836c19e
Manish <manish.jagtap@xelpmoc.in>
2024-04-11 14:49:15 +0530

    Added Latest Code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 require('dotenv').config()
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 const postsRouter = require('./routes/posts.router')
@@ -12,7 +12,16 @@ const authRouter = require('./routes/auth.router')
 app.use("/api/v1/posts", postsRouter)
 app.use("/api/v1/auth", authRouter)
 
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Server Working Fine" });
+});
+
 const PORT = process.env.PORT || 5000
+
+app.use((req, res, next) => {
+    console.log(`${new Date().toISOString()}: ${req.method} ${req.url}`);
+    next();
+});
 
 app.listen(PORT, () => {
     console.log("Server is running....")
